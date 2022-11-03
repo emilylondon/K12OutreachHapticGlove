3d5fddf5c91e48235072f1b550ba5fb6a90ab5b3
Emily London <emilylondon@Emilys-MBP.lan>
2022-11-02 21:35:49 -0700

    Object classifier

---
+++ README.md
@@ -0,0 +1,5 @@
+# Installation guide 
+
+## To open Jupyter Notebook on M1 Mac 
+
+`python3 -m notebook`
---
+++ predict.py
@@ -0,0 +1,21 @@
+import pickle
+from flask import Flask,request
+import json
+
+clf = pickle.load(open('model.pickle','rb'))
+
+
+app = Flask(__name__)
+
+@app.route("/classify")
+def hello_world():
+    sensor1 = float(request.args.get('s1'))
+    sensor2 = float(request.args.get('s2'))
+    sensor3 = float(request.args.get('s3'))
+    sensor4 = float(request.args.get('s4'))
+    sensor5 = float(request.args.get('s5'))
+    sensor6 = float(request.args.get('s6'))
+
+    object = int(clf.predict([[sensor1, sensor2, sensor3, sensor4, sensor5, sensor6]])[0])
+    #print(denomination)
+    return json.dumps({"sensor1":sensor1, "sensor2":sensor2 , "sensor3":sensor3, "sensor4":sensor4, "sensor5":sensor5, "sensor6": sensor6, "object":object})
